4f6d59a7ee96ea9c98a6b83a52afad893193f14f
tofayel <tofayeltuhin143@gmail.com>
2025-12-14 14:14:59 +0600

    feat: add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="text-center mt-10 pb-5 text-retro-magenta">
+      © {new Date().getFullYear()} Miskat Showmik. All rights reserved.
+    </footer>
+  )
+}
+
+export default Footer
+
